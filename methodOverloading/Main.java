290874e803903a7be4b10b79620a6d37bae91417
Adapanavya <151008056+Adapanavya@users.noreply.github.com>
2023-12-11 10:44:25 +0530

    Delete methodOverloading directory

--- methodOverloading/Main.java
+++
@@ -1,9 +0,0 @@
-package com.navya.corejava.methodOverloading;
-
-public class Main {
-    public static void main(String[] args) {
-        MethodOverloading m = new MethodOverloading();
-        m.add(1,2 );
-        m.add(1,2,3 );
-    }
-}
