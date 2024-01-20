7979e0fc193b8de78f9f67b8c95a6d072fddf951
Adapanavya <151008056+Adapanavya@users.noreply.github.com>
2024-01-20 18:52:11 +0530

    Update Main.java

--- com/navya/corejava/generics/generic1/Main.java
+++ com/navya/corejava/generics/generic1/Main.java
@@ -5,7 +5,7 @@ import static com.java.practice.generic1.Student.*;
 public class Main {
     public static void main(String [] args){
         Student<String,Integer> studentName=new Student<>("navya",1);
-        String str=studentName.getName();
+        //String str=studentName.getName();
         Student.printData(studentName);
         System.out.println(str);
     }
